34e3ee99d268c5e8539eea6956e18828af7a2f8e
ganmin1996 <er159252@gmail.com>
2021-02-25 07:39:17 +0900

    express hello world!

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("hello");
+const express = require("express");
+
+const app = express();
+const PORT = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${PORT}`);
+});
